e14da1dd71b8fb4c78cdbc20eaa07a57b5e56ae2
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2025-08-06 13:42:37 +0530

    docs: regular changes

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,6 @@ import Github from './pages/Github';
 
 export const routes = [
  
-  { path: '/todo', element: <Todo /> },
-  { path: '/github', element: <Github /> },
+  { path: '/todo', element: Todo },
+  { path: '/github', element: Github },
 ];
